25cf564125952f47019e7b12cb67e6d7d1258854
seungminRyu <fbtmdals011@naver.com>
2021-08-19 01:32:19 +0900

    init react app

---
+++ stock-sye/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>stock eye</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
