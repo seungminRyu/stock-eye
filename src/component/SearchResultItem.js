5a48cdcc30119c1712dc4a28d26e52ba7f3824e5
seungminRyu <fbtmdals011@naver.com>
2021-08-26 23:13:30 +0900

    Edit: 주식 추가시 로컬 스토리지 업데이트

--- src/component/SearchResultItem.js
+++ src/component/SearchResultItem.js
@@ -4,7 +4,17 @@ import { useStockDispatch } from '../context/StockContext';
 function SearchResultItem(props) {
     const { stock } = props;
     const dispatch = useStockDispatch();
-    const onAdd = () => dispatch({ type: 'ADD', stock });
+    const onAdd = () => {
+        const updateLocalStorage = () => {
+            const nextStockList = JSON.parse(localStorage.getItem('STOCK_LIST')).concat(stock);
+            localStorage.setItem('STOCK_LIST', JSON.stringify(nextStockList));
+        }
+
+        const addStateItem = () => dispatch({ type: 'ADD', stock });
+        
+        addStateItem();
+        updateLocalStorage();
+    };
 
     return (
         <li className="search-result-item">
