9e17922f72a07ed4ad3531ad710eabe7a3f40008
seungminRyu <fbtmdals011@naver.com>
2021-09-09 00:53:29 +0900

    Create: Stock 컴포넌트 추가

---
+++ src/component/Stock.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppTemplate from './AppTemplate';
+
+function Stock() {
+    return (
+        <AppTemplate>
+            <h1>주식 자세히</h1>
+        </AppTemplate>
+    );
+}
+
+export default Stock;
