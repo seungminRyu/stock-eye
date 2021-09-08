e1f512fc714d4e76717106a6812c9df5de38f4f6
seungminRyu <fbtmdals011@naver.com>
2021-09-09 00:52:58 +0900

    Create: 메인 페이지 Home 컴포넌트화

---
+++ src/component/Home.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Search from './Search';
+import Manage from './Manage';
+import UserStocks from './UserStocks';
+import Header from './Header';
+import AppTemplate from './AppTemplate';
+
+function App() {
+    const [isManageOpen, setIsManageOpen] = useState(false);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+    const onManageOpen = () => {
+        const activateManage = () => setIsManageOpen(true);
+        activateManage();
+    }
+
+    const onManageQuit = () => {
+        const deactivateManage = () => setIsManageOpen(false);
+        deactivateManage();
+    }
+
+    const onSearchOpen = () => {
+        const activateSearch = () => setIsSearchOpen(true);
+        activateSearch();
+    }
+
+    return (
+        <AppTemplate>
+            <Header/>
+            <UserStocks
+                onManageOpen={onManageOpen}
+                onSearchOpen={onSearchOpen}
+            />
+            <Manage
+                isManageOpen={isManageOpen}
+                onManageQuit={onManageQuit}
+            />
+            <Search
+                isSearchOpen={isSearchOpen}
+                setIsSearchOpen={setIsSearchOpen}
+            />
+        </AppTemplate>
+    );
+}
+
+export default App;
