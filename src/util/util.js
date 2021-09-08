7de7fce04f1e6f8f29cf1ca1211f40576770397a
seungminRyu <fbtmdals011@naver.com>
2021-09-09 01:22:35 +0900

    Add: url 쿼리 파싱 util 메서드 추가

--- src/util/util.js
+++ src/util/util.js
@@ -6,3 +6,14 @@ export const debouncer = (time, callback) => {
     }
     timer = setTimeout(callback, time);
 }
+
+export const parseQueryString = (queryString) => {
+    const queryList = queryString[0] === "?" ? queryString.slice(1).split("&") : queryString.split("&");
+    let ret = {};
+    queryList.forEach(query => {
+        const [key, value] = query.split("=");
+        ret[key] = value;
+    });
+
+    return ret;
+}
