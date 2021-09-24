fdb0eaa373dfeba609da56955aeb36811db3f1fe
seungminRyu <fbtmdals011@naver.com>
2021-09-25 03:42:40 +0900

    Edit: isAdded를 state로 변경

--- src/component/SearchResultItem.js
+++ src/component/SearchResultItem.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { useStockDispatch } from '../context/StockContext';
+import { getLocalStorageItem } from '../lib/util';
 
 function SearchResultItem({stock}) {
     const { name, code } = stock;
     const dispatch = useStockDispatch();
-
-    const isAdded = JSON.parse(localStorage.getItem('STOCK_LIST')).findIndex(stock => 
-        stock.code === code
-    ) === -1 ? false : true;
+    const [isAdded, setIsAdded] = useState(
+        getLocalStorageItem('STOCK_LIST').findIndex(stock => stock.code === code)
+            === -1 ? false : true
+    );
 
     const onAdd = () => {
         const updateLocalStorage = () => {
@@ -20,6 +21,7 @@ function SearchResultItem({stock}) {
         
         addStateItem();
         updateLocalStorage();
+        setIsAdded(!isAdded);
     };
 
     return (
@@ -29,7 +31,7 @@ function SearchResultItem({stock}) {
                 <p className="stock-name">{ name }</p>
             </StockLabel>
             { isAdded ?
-                <button className="exist-btn">추가 됨</button> :
+                <Added>추가 됨</Added> :
                 <AddButton onClick={onAdd}>추가하기</AddButton>
             }
         </SearchResultItemBlock>
@@ -60,12 +62,28 @@ const StockLabel = styled.div`
 `;
 
 const AddButton = styled.button`
+    display: grid;
+    place-items: center;
+    width: 68px;
+    height: 36px;
     font-size: 12px;
     font-weight: 600;
     color: var(--white);
     background-color: var(--main);
     border-radius: 8px;
-    padding: 12px 12px 11px;
+`;
+
+const Added = styled.button`
+    display: grid;
+    place-items: center;
+    width: 68px;
+    height: 36px;
+    font-size: 12px;
+    font-weight: 600;
+    color: var(--main);
+    border: solid 1px var(--main);
+    border-radius: 8px;
+    cursor: unset;
 `;
 
 export default SearchResultItem;
