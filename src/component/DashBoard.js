484a731e5999267662b7a70424afe95e7487d0bf
seungminRyu <fbtmdals011@naver.com>
2021-09-24 22:19:23 +0900

    Create: DashBoard 컴포넌트 분리

---
+++ src/component/DashBoard.js
@@ -0,0 +1,97 @@
+import React from "react";
+import styled from 'styled-components';
+
+import icoSearch from '../static/asset/ico_search.svg';
+import icoManage from '../static/asset/ico_manage.svg';
+
+function DashBoard(props) {
+    const {
+        onSearchOpen,
+        onManageOpen,
+        stockList
+    } = props;
+    const stockNum = stockList.length;
+
+    return (
+        <DashBoardBlock>
+            <h2 className="section-title">유승민 님의 Stock-eye</h2>
+            <div className="dashboard-wrapper">
+                <StockNum>
+                    <p>현재 관리중인 주식:</p>
+                    <p className="num-text">{stockNum} 개</p>
+                </StockNum>
+                <Menu>
+                    <MenuButton className="search-btn" onClick={onSearchOpen}>
+                        <span>주식추가</span>
+                    </MenuButton>
+                    <div className="split-bar"></div>
+                    <MenuButton className="manage-btn" onClick={onManageOpen}>
+                        <span>내 주식관리</span>
+                    </MenuButton>
+                </Menu>
+            </div>
+        </DashBoardBlock>
+    )
+}
+
+const DashBoardBlock = styled.div`
+    padding: 40px 24px 0;
+
+    .dashboard-wrapper {
+        width: 100%;
+        border-radius: 24px;
+        background-color: var(--bg-gray);
+        padding: 24px 24px 0;
+    }
+`;
+
+const StockNum = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    font-size: 15px;
+    border-bottom: solid 1px var(--border);
+    padding-bottom: 20px;
+
+    .num-text {
+        font-size: 16px;
+        font-weight: 600;
+        text-align: end;
+    }
+`;
+
+const Menu = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1px 1fr;
+    place-content: center;
+
+    .split-bar {
+        width: 1px;
+        height: 10px;
+        margin-top: 23px;
+        background-color: var(--border);
+    }
+`;
+
+const MenuButton = styled.button`
+    width: 100%;
+    height: 100%;
+    font-size: 15px;
+    padding: 20px 0 24px;
+
+    & span {
+        background-repeat: no-repeat;
+        background-size: 20px 20px;
+        background-position: 0 center;
+        padding-left: 24px;
+    }
+
+    &.search-btn span {
+        background-image: url(${icoSearch});
+    }
+
+    &.manage-btn span {
+        background-image: url(${icoManage});
+    }
+`;
+
+export default DashBoard;
--- src/component/Main.js
+++ src/component/Main.js
@@ -3,9 +3,8 @@ import styled from 'styled-components';
 import { useStockState } from '../context/StockContext';
 import StockList from './StockList';
 import PredictList from './PredictList';
+import DashBoard from './DashBoard';
 
-import icoSearch from '../static/asset/ico_search.svg';
-import icoManage from '../static/asset/ico_manage.svg';
 
 function Main(props) {
     const {
@@ -13,33 +12,16 @@ function Main(props) {
         onManageOpen
     } = props;
     const stockList = useStockState();
-    const stockNum = stockList.length;
 
     return (
         <MainBlock>
-            <DashBoardBody>
-                <h2 className="section-title">유승민 님의 Stock-eye</h2>
-                <DashBoardBody>
-                    <StockNum>
-                        <p>현재 관리중인 주식:</p>
-                        <p className="num-text">{stockNum} 개</p>
-                    </StockNum>
-                    <Menu>
-                        <MenuButton className="search-btn" onClick={onSearchOpen}>
-                            <span>주식추가</span>
-                        </MenuButton>
-                        <div className="split-bar"></div>
-                        <MenuButton className="manage-btn" onClick={onManageOpen}>
-                            <span>내 주식관리</span>
-                        </MenuButton>
-                    </Menu>
-                </DashBoardBody>
-            </DashBoardBody>
+            <DashBoard 
+                onManageOpen={onManageOpen}
+                onSearchOpen={onSearchOpen}
+                stockList={stockList}
+            />
             <PredictList/>
-            <UserStockList>
-                <h2 className="section-title">주식 목록</h2>
-                <StockList stockList={stockList}/>
-            </UserStockList>
+            <StockList stockList={stockList}/>
         </MainBlock>
     )
 }
@@ -57,73 +39,6 @@ const MainBlock = styled.main`
         font-weight: 600;
         margin-bottom: 16px;
     }
-    
-`;
-
-const DashBoardBody = styled.div`
-    padding: 40px 24px 0;
-`;
-
-const DashBoardBody = styled.div`
-    width: 100%;
-    border-radius: 24px;
-    background-color: var(--bg-gray);
-    padding: 24px 24px 0;
-`;
-
-const StockNum = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    font-size: 15px;
-    border-bottom: solid 1px var(--border);
-    padding-bottom: 20px;
-
-    .num-text {
-        font-size: 16px;
-        font-weight: 600;
-        text-align: end;
-    }
-`;
-
-const Menu = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1px 1fr;
-    place-content: center;
-
-    .split-bar {
-        width: 1px;
-        height: 10px;
-        margin-top: 23px;
-        background-color: var(--border);
-    }
-`;
-
-const MenuButton = styled.button`
-    width: 100%;
-    height: 100%;
-    font-size: 15px;
-    padding: 20px 0 24px;
-
-    & span {
-        background-repeat: no-repeat;
-        background-size: 20px 20px;
-        background-position: 0 center;
-        padding-left: 24px;
-    }
-
-    &.search-btn span {
-        background-image: url(${icoSearch});
-    }
-
-    &.manage-btn span {
-        background-image: url(${icoManage});
-    }
-`;
-
-const UserStockList = styled.div`
-    background-color: var(--bg-white);
-    padding: 0 24px 80px;
-    margin-top: 40px;
 `;
 
 export default Main;
