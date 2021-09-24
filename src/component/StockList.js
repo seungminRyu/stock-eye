a03a6e5432d569ccc723748958ede559c86fe5b9
seungminRyu <fbtmdals011@naver.com>
2021-09-24 22:20:42 +0900

    Edit: StockList에 헤더 추가

--- src/component/StockList.js
+++ src/component/StockList.js
@@ -3,13 +3,12 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import StockItem from './StockItem';
 
-const StockListBlock = styled.div``;
-
 function StockList(props) {
     const { stockList } = props;
 
     return (
         <StockListBlock>
+            <h2 className="section-title">주식 목록</h2>
             <ul className="user-stock-list">
                 {stockList.map((stock, i) =>
                     <Link to={`/stock?name=${stock.name}`} key={stock.code}>
@@ -20,5 +19,11 @@ function StockList(props) {
         </StockListBlock>
     );
 }
+
+const StockListBlock = styled.div`
+    background-color: var(--bg-white);
+    padding: 0 24px 80px;
+    margin-top: 40px;
+`;
 
 export default StockList;
