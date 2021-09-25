00c6a4d80d4882f4856dc6d1471f7d860dd7824c
seungminRyu <fbtmdals011@naver.com>
2021-09-26 05:58:27 +0900

    Add: Predict 페이지에서 로컬 예측정보 가져옴

--- src/page/Predict.js
+++ src/page/Predict.js
@@ -4,12 +4,19 @@ import useAsync from "../hook/useAsync";
 import { fetchPredictData } from "../lib/api";
 import { getLocalStorageItem, parseQueryString } from "../lib/util";
 
+const getStockName = url => {
+    const queryObj = parseQueryString(url);
+    return decodeURIComponent(queryObj.name);
+}
+
+const getTargetPredictItem = name => {
+    return getLocalStorageItem('PREDICT_LIST').filter(item => item.name === name)[0];
+}
+
 function Predict({ location }) {
-    const queryObj = parseQueryString(location.search);
-    const name = decodeURIComponent(queryObj.name);
-    console.log(name);
-    const { id } = getLocalStorageItem('PREDICT_LIST').filter(item => item.name === name)[0];
-    console.log(id )
+    const name = getStockName(location.search);
+    const { id, predictDate, startDate, startVals } = getTargetPredictItem(name);
+    console.log(getTargetPredictItem(name));
     const [state, refetch] = useAsync({
         callback: fetchPredictData,
         params: [name, id]
