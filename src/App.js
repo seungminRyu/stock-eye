e63ce185ad155b9dab0bcd0b291c1a75b545fead
seungminRyu <fbtmdals011@naver.com>
2021-10-11 20:13:01 +0900

    Add: serviceWorker 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { createGlobalStyle } from 'styled-components';
-import { Route } from 'react-router';
-import { getLocalStorageItem } from './lib/util';
-import { requestQueueInStocks } from './lib/api';
-import { StockProvider } from './context/StockContext';
-import Home from './page/Home';
-import Stock from './page/Stock';
-import Predict from './page/Predict';
+import React, { useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
+import { Route } from "react-router";
+import { getLocalStorageItem } from "./lib/util";
+import { requestQueueInStocks } from "./lib/api";
+import { StockProvider } from "./context/StockContext";
+import Home from "./page/Home";
+import Stock from "./page/Stock";
+import Predict from "./page/Predict";
 
 function App() {
     initPredictList();
@@ -15,23 +15,23 @@ function App() {
         await requestQueueInStocks(stockList);
 
         return () => console.log("gone");
-    }, [])
+    }, []);
 
     return (
         <StockProvider>
-            <GlobalStyle/>
-            <Route path="/" exact={true} component={Home}/>
-            <Route path="/stock" component={Stock}/>
-            <Route path="/predict" component={Predict}/>
+            <GlobalStyle />
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/stock" component={Stock} />
+            <Route path="/predict" component={Predict} />
         </StockProvider>
     );
 }
 
 const initPredictList = () => {
-    if (localStorage.getItem('PREDICT_LIST') === null) {
-        localStorage.setItem('PREDICT_LIST', JSON.stringify([]));
+    if (localStorage.getItem("PREDICT_LIST") === null) {
+        localStorage.setItem("PREDICT_LIST", JSON.stringify([]));
     }
-}
+};
 
 const GlobalStyle = createGlobalStyle`
     body {
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>
-    , document.getElementById('root')
+    </BrowserRouter>,
+    document.getElementById("root")
 );
+serviceWorker.register();
