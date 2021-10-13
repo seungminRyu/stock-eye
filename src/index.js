e16a787c8b50a986979bbdf680bbe39cdec68d24
seungminRyu <56435483+seungminRyu@users.noreply.github.com>
2021-10-13 13:05:19 +0900

    Remove: App에 있는 serviceWorker 삭제

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
+import {BrowserRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
@@ -11,4 +10,3 @@ ReactDOM.render(
     </BrowserRouter>,
     document.getElementById("root")
 );
-serviceWorker.register();
